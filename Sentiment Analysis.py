002e0285989bc4ba9172c6f59542a5b05564a80a
BrianCanyon <brian.canyon@gmail.com>
2025-01-27 14:57:14 -0800

    Continued dev work
    Some text cleaning and prep for sentiment analysis

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -14,11 +14,44 @@ order to remove the need for a DB connection
 ## Libraries
 from sqlalchemy import create_engine, text
 import pandas as pd
+import yfinance as yf
+# from sklearn.feature_extraction.text import TfidfVectorizer # For TF-IDF
 
-## Pulling data from sql to pandas
-engine = create_engine('mysql+mysqlconnector://root:@localhost/mysql')
-query = 'SELECT * FROM posts'
-
+## Pulling data from sql to pandas df
+engine = create_engine('mysql+mysqlconnector://root:59shredder@localhost/mysql')
+query = text('SELECT * FROM posts')
 posts_df = pd.read_sql(query, engine)
-print(posts_df)
+
+# The df from the posts table is very raw, below creates a column that determines
+# if each post is about a specific stock/crypto/ect., then drops what cant be 
+# indentified
+
+# Get list of ticker symbols and names from F500
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+tables = pd.read_html(url)
+sp500_table = tables[0]
+stock_tickers = sp500_table['Symbol'].to_list()
+stock_names = sp500_table['Security'].to_list()
+stock_list = stock_tickers + stock_names
+stock_list.extend(['BTC', 'Bitcoin'])
+
+# Drop rows that do not relate to a specific F500 stock or have empty text body
+indexes_to_keep = set()
+for index, row in posts_df.iterrows():
+    for stock in stock_list:
+        if stock in row['title'] and row['text_body'] != None:
+            indexes_to_keep.add(index)
+
+posts_df = posts_df.loc[posts_df.index.isin(indexes_to_keep)]
+for title in posts_df['title'][:40]:
+    print(title)
+
+
+
+
+            
+
+    
+
+
 
