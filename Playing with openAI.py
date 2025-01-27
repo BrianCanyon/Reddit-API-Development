7d9d316705acef4c5da82576684b97cf6b45d730
BrianCanyon <brian.canyon@gmail.com>
2025-01-27 15:03:19 -0800

    OpenAI fun

---
+++ Playing with openAI.py
@@ -0,0 +1,15 @@
+## Playing with openAI
+import openai
+
+with open("/Users/brian.canyon.Documents/openAI.key.txt")as file:
+    key = file.read()
+
+response = openai.Completion.create(
+    engine ='gpt-3.5-turbo',
+    prompt = 'What is the circumfrance of earth',
+    max_tokens = 10
+)
+
+answer = response.choices[0].text.strip()
+
+print(answer)
